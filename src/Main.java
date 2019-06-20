90baa90b6a1897db3c3a7124c7857bdfc7277571
ansumana80 <ansurogersbusiness@gmail.com>
2019-06-20 18:04:37 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Collections;
+import java.util.ArrayList;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Double> temperatureList = new ArrayList<Double>();
+        //Adding items to your ArrayList is simple and fun with the add method.
+        temperatureList.add(40.5);
+        temperatureList.add(33.9);
+        temperatureList.add(37.8);
+        temperatureList.add(15.3);
+        temperatureList.add(25.6);
+
+        System.out.println(temperatureList);
+
+        for (Double temp:temperatureList)
+        {
+            System.out.println(temp);
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
+
+
